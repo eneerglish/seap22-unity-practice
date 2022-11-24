1640b998806b848e2e84b430b9bfafff99fa9a9d
Kossy3 <ykoshi873@gmail.com>
2022-11-24 17:25:27 +0900

    キー入力で左右に移動できるようにMoveSheepを改良 変数speed を定義しinspectorから調節できるように改良 坂を上るときに摩擦でひつじが回転しないように、 RigidBody2D > Constraints > FreezeRotation > Z にチェックを入れる

--- Assets/MoveSheep.cs
+++ Assets/MoveSheep.cs
@@ -6,6 +6,7 @@ public class MoveSheep : MonoBehaviour
 {
     // privateのメンバ は一般にキャメルケース (最初小文字 + 大文字区切り)
     private Rigidbody2D rigidBody = null;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,19 @@ public class MoveSheep : MonoBehaviour
         // 変数を作る (メソッド内のみで使う変数)
         // rigidbodyの物理演算を使うためにはtransformではなくrigidbodyを使って移動する必要がある。
         Vector2 pos = rigidBody.position;
-        // 前に少しずつ移動するように加算
-        pos += new Vector2(0.05f,0.0f);
-        // positionを更新
+
+        //キー入力によってposの値を変える
+
+        // 左に移動
+        if (Input.GetKey (KeyCode.A)) {
+            pos += new Vector2(-speed, 0.0f);
+        }
+        // 右に移動
+        if (Input.GetKey (KeyCode.D)) {
+            pos += new Vector2(speed, 0.0f);
+        }
+
+        // position を更新
         rigidBody.position = pos;
     }
 }

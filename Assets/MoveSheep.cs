102705c35b2603826908f0015b16e8c8943f7829
Kossy3 <ykoshi873@gmail.com>
2022-12-03 01:34:31 +0900

    MoveSheepにジャンプ機能を追加

--- Assets/MoveSheep.cs
+++ Assets/MoveSheep.cs
@@ -6,13 +6,16 @@ public class MoveSheep : MonoBehaviour
 {
     // privateのメンバ は一般にキャメルケース (最初小文字 + 大文字区切り)
     private Rigidbody2D rigidBody = null;
+    private GroundCheck groundCheck = null;
     public float speed;
+    public float jump;
 
     // Start is called before the first frame update
     void Start()
     {
         // Updateでいちいち取得すると重いので,代入して保存しておく
         rigidBody = this.GetComponent<Rigidbody2D>();
+        groundCheck = this.GetComponentInChildren<GroundCheck>();
 
     }
 
@@ -32,6 +35,13 @@ public class MoveSheep : MonoBehaviour
         // 右に移動
         if (Input.GetKey (KeyCode.D)) {
             pos += new Vector2(speed, 0.0f);
+        }
+
+        //ジャンプ
+        if (Input.GetKeyDown (KeyCode.Space) && groundCheck.isGround) {
+            //AddForceは物理演算でオブジェクトに力を加えるメソッド
+            //ForceModeは力の加え方のオプション Impulseは瞬間的に力を与える
+            rigidBody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
         }
 
         // position を更新

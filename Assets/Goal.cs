5a832d28e4602f66880e07fb7a1994d751c5649b
SHIKINAMl <107552771+SHIKINAMl@users.noreply.github.com>
2022-12-18 18:07:23 +0900

    Goalスクリプトを編集。ひつじに触れたらScoreシーンに移るように

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (col.name == "hitsuji_dot")
+        {
+            SceneManager.LoadScene("Score");
+        }
     }
 }

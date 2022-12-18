82da7fbd6b4c143617dd2a187d1433eb5b0b2df1
SHIKINAMl <107552771+SHIKINAMl@users.noreply.github.com>
2022-12-18 17:07:24 +0900

    経過時間を秒表記で表示するように変更

--- Assets/TimeScore.cs
+++ Assets/TimeScore.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeScore : MonoBehaviour
 {
+    private Text scoreText = null;
+    private float totalTime = 0f;
+    private int seconds = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = this.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        totalTime += Time.deltaTime;
+        seconds = (int)totalTime;
+        scoreText.text = seconds.ToString();
     }
 }
